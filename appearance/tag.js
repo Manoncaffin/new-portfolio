176b73f3758a1993e77f301b77904f6349ffa5fe
Caffin <caffinmanon@gmail.com>
2024-10-10 19:57:44 +0200

    dropdown

--- appearance/tag.js
+++ appearance/tag.js
@@ -2,21 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const tags = document.querySelectorAll('.tag-mobile, .tag-desktop');
     const projects = document.querySelectorAll('.project');
     const section = document.querySelector('.section');
+    let activeTag = null;
 
     tags.forEach(tag => {
         tag.addEventListener('click', () => {
             const category = tag.getAttribute('data-category');
 
-            tags.forEach(t => t.classList.remove('active'));
-            tag.classList.add('active');
-
-            projects.forEach(project => {
-                if (category === 'all' || project.classList.contains(category)) {
+            if (activeTag === tag) {
+                tags.forEach(t => t.classList.remove('active'));
+                activeTag = null; 
+                projects.forEach(project => {
                     project.classList.remove('hidden'); 
-                } else {
-                    project.classList.add('hidden'); 
-                }
-            });
+                });
+            } else {
+                tags.forEach(t => t.classList.remove('active'));
+                tag.classList.add('active');
+                activeTag = tag;
+
+                projects.forEach(project => {
+                    if (category === 'all' || project.classList.contains(category)) {
+                        project.classList.remove('hidden'); 
+                    } else {
+                        project.classList.add('hidden'); 
+                    }
+                });
+            }
 
             adjustGrid(); 
         });
